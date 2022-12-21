916bd32b45a01b6796027464a3b61bff30fc4357
Joshua Peterson <jpeterson2058@gmail.com>
2022-12-21 13:12:50 -0700

    New input

--- dynamicDifficulty.py
+++ dynamicDifficulty.py
@@ -5,7 +5,6 @@ import datetime as datetime
 from dataclasses import dataclass
 from typing import Any, List
 import pandas as pd
-import datetime as datetime
 import hashlib
 
 @dataclass
@@ -14,6 +13,8 @@ class Block:
     creator_id: int
     prev_hash: str = 0
     timestamp: str = datetime.datetime.utcnow().strftime("%H:%M:%S")
+    prev_hash: str = 0
+    nonce: int = 0
 
     def hash_block(self):
         sha = hashlib.sha256()
@@ -21,11 +22,18 @@ class Block:
         data = str(self.data).encode()
         sha.update(data)
 
-        timestamp = str(self.timestamp).encode()
-        sha.update(timestamp)
+        creator_id = str(self.creator_id).encode()
 
         prev_hash = str(self.prev_hash).encode()
         sha.update(prev_hash)
+
+        sha.update(data)
+
+        timestamp = str(self.timestamp).encode()
+        sha.update(timestamp)
+
+        nonce = str(self.nonce).encode()
+        sha.update(nonce)
 
         return sha.hexdigest()
 
@@ -33,8 +41,22 @@ class Block:
 @dataclass
 class PyChain:
     chain: list[Block]
+    difficulty: int = 4
 
-    def add_block(self, block):
+    def proof_of_work(self,block):
+        calculated_hash = block.hash_block()
+
+        num_of_zeros = "0" * self.difficulty
+
+        while not calculated_hash.startswith(num_of_zeros):
+            block.nonce += 1
+            calculated_hash = block.hash_block()
+
+        print("Winning Hash", calculated_hash)
+        return block        
+
+    def add_block(self, candidate_block):
+        block = self.proof_of_work(candidate_block)
         self.chain += [block]
 
 # Streamlit code
@@ -48,34 +70,29 @@ def setup():
 
 pychain = setup()
 
-st.markdown("# PyChain: A Python Blockchain Application")
+st.markdown("# PyChain")
 st.markdown("## Store Data in the Chain")
+
+# Add a Streamlit slider named "Block Difficulty" that allows the user to update a difficulty value.  Set this equal to the variable "difficulty 1,5,4"
+difficulty = st.sidebar.slider("Block Difficulty", 1, 5, 4)
+
+# Update the difficulty data attribute of the Pychain data class (pychain.difficulty) with the new difficulty value
+pychain.difficulty = difficulty
 
 input_data = st.text_input("Block Data")
 
 if st.button("Add Block"):
-    # @TODO:
     # Select the previous block in the chain
     prev_block = pychain.chain[-1]
-
-    # @TODO
-    # Hash the previous block in the chain
     prev_block_hash = prev_block.hash_block()
 
-    # @TODO
-    # Create a block in the new chain
     new_block = Block(data="new_block", creator_id=42, prev_hash=prev_block_hash)
 
-    # @TODO
-    # Add the new block to the chain
     pychain.add_block(new_block)
 
-st.markdown("## Pychain Ledger")
+    st.write("Winning Hash", new_block.hash_block())
 
-# @TODO
-# Create a Pandas DataFrame to display the PyChain ledger
+st.markdown("## Pychain Ledger")
 pychain_df = pd.DataFrame(pychain.chain)
 
-# @TODO
-# Use the streamlit write function to display the PyChaine dataframe
-st.write(pychain_df)
+st.write(pychain_df)        
